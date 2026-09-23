e8543f7d659490f4119b5a5d99bdca03555c9ff2
JahamisD <jamesartinosanto77@gmail.com>
2026-09-24 00:42:27 +0800

    order discount - Inosanto.JamesArthur

--- tickets.js
+++ tickets.js
@@ -2,12 +2,18 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
+function calculateTicketPrice(quantity, basePrice, isPremium = false) {
   let price = quantity * basePrice;
 
   if (quantity >= 5) {
     price *= 0.9;
   }
+
+  if (isPremium) {
+    price *= 1.5;
+  }
+
+  price -= 10;
 
   return Math.round(price);
 }
